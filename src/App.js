d3bff13cfb967704bc91604bb0f80cee755926dd
Ishita <ishita.k@turvo.com>
2024-08-10 14:47:29 +0530

    Added react router dom and created route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Routes from './Routes';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes />
     </div>
   );
 }
